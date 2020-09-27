8c264e4ce15360b72aa0af19e7e05e5610365da3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-27 14:27:19 -0700

    Add state needed to show information on the page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,42 @@ import AccountSection from './components/AccountSection/AccountSection';
 import ExpensesSection from './components/ExpensesSection/ExpensesSection';
 
 const App = () => {
+  const [expense, setExpense] = React.useState([
+    { expenseID: 1, expense: 49.90, locationName: 'Whole Foods', date: '2020-15-03', accountId: '', categoryId: ''}
+  ]);
+  const [account, setAccount] = React.useState([
+     { accountID: 20, accountTitle: 'groceries', color: '', type: 'bank account' }
+  ]);
+  const [category, setCategory] = React.useState([
+     { categoryID: 15, categoryTitle: 'groceries', color: '' }
+  ]);
+  const colorTypes = [
+    {type: 'bank account', color: ''},
+    {type: 'card', color: ''},
+    {type: 'credit', color: ''}
+  ];
+
+  const addExpense = (expense) => {
+    setExpense(expense);
+  }
+
+  const addAccount = (account) => {
+    setAccount(account);
+  }
+
+  const addCategory = (category) => {
+    setCategory(category);
+  }
 
   return (
     <section className='App'>
       <NavBar />
       <section>
-         <CategorySection />
+         <CategorySection categories={category} addCategory={addCategory} />
       </section>
       <section>
-        <AccountSection />
-        <ExpensesSection />
+        <AccountSection accounts={account} addAccount={addAccount}/>
+        <ExpensesSection expenses={expense} addExpense={addExpense} />
       </section>
     </section>
   );
