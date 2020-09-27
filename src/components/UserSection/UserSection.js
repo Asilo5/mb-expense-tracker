d064e2a5ec84efb69e0ed44104b66375fa0bbc7f
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-27 15:05:08 -0700

    Add import of css

--- src/components/UserSection/UserSection.js
+++ src/components/UserSection/UserSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './UserSection.css';
 
 const UserSection = () => {
     return (
-        <section>
+        <section className='user-section'>
             <img src='https://api.time.com/wp-content/uploads/2017/07/1f9dd-w-5-320px.png' alt='user image'/>
             <p>Salome Lopez</p>
         </section>
