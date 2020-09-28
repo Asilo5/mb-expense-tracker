e1c595cda40c312fe8da8b7c13f6029e69dd4b1a
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-27 21:07:07 -0700

    Updates

---
+++ precache-manifest.02a4d74bbb841b0c607c549a7da5e0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0c38bc845d82c3a5c0cbd940196f97",
+    "url": "/mb-expense-tracker/index.html"
+  },
+  {
+    "revision": "9e305992470758ad059f",
+    "url": "/mb-expense-tracker/static/css/main.6ed9bdc9.chunk.css"
+  },
+  {
+    "revision": "a06fe6a43bcec9727512",
+    "url": "/mb-expense-tracker/static/js/2.5890ff16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mb-expense-tracker/static/js/2.5890ff16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e305992470758ad059f",
+    "url": "/mb-expense-tracker/static/js/main.53fce506.chunk.js"
+  },
+  {
+    "revision": "d436e457bf1d9c6942c2",
+    "url": "/mb-expense-tracker/static/js/runtime-main.142aad8e.js"
+  }
+]);
